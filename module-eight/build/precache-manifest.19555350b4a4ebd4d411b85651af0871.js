98023a37ad9fcb0ffb2353e41a4c5ce80725ee74
analimazn <analimazn@gmail.com>
2020-06-17 23:57:13 -0300

    changed names of directories

---
+++ module-eight/build/precache-manifest.19555350b4a4ebd4d411b85651af0871.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a418188ed84c0a581d85187d51f3cfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f4aaaaa6f5a74bb1435",
+    "url": "/static/css/main.8e3f1c05.chunk.css"
+  },
+  {
+    "revision": "688a68bcbf44d16b2fd5",
+    "url": "/static/js/2.6493f19d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6493f19d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4aaaaa6f5a74bb1435",
+    "url": "/static/js/main.a7cfc136.chunk.js"
+  },
+  {
+    "revision": "46d026360425f15baf8e",
+    "url": "/static/js/runtime-main.96b555da.js"
+  },
+  {
+    "revision": "1ff53bddbdfc9180c33167ee29befa58",
+    "url": "/static/media/instagram-logo.1ff53bdd.svg"
+  }
+]);
